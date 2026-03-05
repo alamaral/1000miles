48d93649b4058ee4574bbd36849a292d84839ba5
Alan Amaral <alan.amaral@avispl.com>
2026-03-05 17:32:05 -0500

    Fix card width

--- client/src/components/PlayerHand.jsx
+++ client/src/components/PlayerHand.jsx
@@ -5,7 +5,7 @@ const MIN_CARDS = 7
 const GAP = 8
 // Available width inside the hand area (design width minus container + hand padding)
 const HAND_INNER = 1700 - 24 - 32
-const CARD_W = Math.floor((HAND_INNER - (MIN_CARDS - 1) * GAP) / MIN_CARDS)
+const CARD_W = Math.floor((HAND_INNER - MIN_CARDS * GAP) / MIN_CARDS)
 const CARD_H = Math.round(CARD_W * (220 / 160))
 
 export default function PlayerHand({ hand, canDrag }) {
